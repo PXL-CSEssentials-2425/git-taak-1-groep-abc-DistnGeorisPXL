c8b6269bc452ab279b68f7dd033e2545beef0e28
DistnGeorisPXL <distn.georis@student.pxl.be>
2024-10-06 15:48:02 +0200

    Hoofdscherm

---
+++ git-taak-1-groep-abc-DistnGeorisPXL/git-taak-1-groep-abc-DistnGeorisPXL/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace git_taak_1_groep_abc_DistnGeorisPXL
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
